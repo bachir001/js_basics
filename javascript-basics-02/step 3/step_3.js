bb66f95bf8a70f28167dbeacfaa935d69f925f6a
bachir <mohamadbachir01@gmail.com>
2021-03-10 10:01:58 +0200

    basics02_step_4

--- javascript-basics-02/step 3/step_3.js
+++ javascript-basics-02/step 3/step_3.js
@@ -5,4 +5,4 @@ var name1 = document.querySelector('#name');
 var divinput_text=document.querySelector('#newtext');
 
 
-name1.addEventListener("change",function changing(){divinput_text.innerHTML=name1.value})
+name1.addEventListener("change",function changing(){divinput_text.innerHTML=name1.value});
